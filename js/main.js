ba02538f0711ad1ede962133ed7d4b56055cc535
Amalie Stokholm <amaliestokholm@gmail.com>
2026-01-11 19:48:18 +0000

    Add more shells

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ starGroup.add(sphere);
 const oscillationMaterials = [];
 oscillationMaterials.push(material);
 
-const shellRadii = [0.95, 0.85, 0.75, 0.65, 0.55];
+const shellRadii = [0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05];
 const shells = [];
 
 shellRadii.forEach((rFrac, index) => {
